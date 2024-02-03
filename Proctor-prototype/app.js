7e553250a2747d852c10bdeffe668901fc00fe9e
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2024-02-03 03:18:43 +0200

    Initial proctor prototype

---
+++ Proctor-prototype/app.js
@@ -0,0 +1,66 @@
+const video = document.getElementById('video');
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo);
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  );
+}
+
+video.addEventListener('play', async () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+
+    const model = await cocoSsd.load();
+
+    const predictions = await model.detect(video);
+
+    predictions.forEach(prediction => {
+      if (prediction.class === 'cell phone') {
+        const ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.rect(prediction.bbox[0], prediction.bbox[1], prediction.bbox[2], prediction.bbox[3]);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'red';
+        ctx.fillStyle = 'red';
+        ctx.stroke();
+
+        ctx.fillText('Phone detected!', prediction.bbox[0], prediction.bbox[1] > 10 ? prediction.bbox[1] - 5 : 10);
+        alert('PUT DOWN YOUR PHONE CHEATER');
+      }
+    });
+
+    if (resizedDetections.length === 0) {
+      console.log('User is absent');
+    } else if (resizedDetections.length > 1) {
+      alert('More than one person detected');
+    }
+
+  }, 100);
+});
+
+document.addEventListener('visibilitychange', (event) => {
+  if (document.visibilityState == 'visible') {
+    console.log('Tab is active');
+  } else {
+    alert('AFSHTK YA CHEATER');
+    console.log('Tab is inactive');
+  }
+});
